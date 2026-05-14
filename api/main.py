df650df24cd5cb2b0e0a425ac0d1608eb647072c
Allen Binoy <allenbinoy888@gmail.com>
2026-05-14 10:38:26 +0530

    Add Module 5 - FastAPI model serving

---
+++ api/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import mlflow.xgboost
+import pandas as pd
+from pydantic import BaseModel
+
+app = FastAPI(title="Churn Prediction API")
+
+# Load model
+MODEL_PATH = "mlruns/1/models/m-19ac7d3b9a2444f8a60e8c1e252e80bb/artifacts"
+model = mlflow.xgboost.load_model(MODEL_PATH)
+
+class Customer(BaseModel):
+    avg_monthly_charges: float
+    avg_tenure_months: float
+    avg_support_tickets: float
+    avg_last_login_days: float
+    avg_num_products: float
+    total_events: int
+
+@app.get("/")
+def root():
+    return {"status": "Churn Prediction API running ✅"}
+
+@app.post("/predict")
+def predict(customer: Customer):
+    data = pd.DataFrame([customer.dict()])
+    prob = model.predict_proba(data)[0][1]
+    risk = "HIGH" if prob > 0.5 else "LOW"
+    return {
+        "churn_probability": round(float(prob), 4),
+        "risk_level": risk,
+        "recommendation": "Contact customer" if risk == "HIGH" else "Monitor only"
+    }
